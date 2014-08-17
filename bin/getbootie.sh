6aad9a81d49daa67789c568fb15926ef31983cf7
Dan Boger <dan@peeron.com>
2014-08-16 17:12:45 -0700

    Avoid double quoting file names, and show what we're doing for debugging.

--- bin/getbootie.sh
+++ bin/getbootie.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 URL=http://bootiemashup.com/blog/category/top10 
 DIR=/home/zigdon/tmp/bootie
@@ -8,13 +8,13 @@ wget -r -l1 -t1 -nd -nc -np -A.mp3 $URL -H -P $DIR -q
 
 cd $DIR
 find . -mtime -10 -type f -print0 |
-  xargs -0 ls -Q -l --time-style=+%m-%Y |
+  xargs -0 ls -l --time-style=+%m-%Y |
   sed 's/  \+/ /g' |
   cut -s -d\  -f6- |
   sed 's/-/ /' |
   while read M Y N; do
     id3v2 --album "Best of Bootie $M-$Y" "$N" --year $Y
-    touch -d "$Y-$M-01" $N;
+    touch -cd "$Y-$M-01" "$N";
   done
 
 cp -n -v *.mp3 $SAVE
