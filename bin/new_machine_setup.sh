7c9d103b16cc69e7713b33d6c0fd0c1d37f5a9a6
Dan Boger <dan@peeron.com>
2015-08-03 15:35:32 -0700

    Update new_machine_setup.sh
    No longer need fetchotp (don't ask)

--- bin/new_machine_setup.sh
+++ bin/new_machine_setup.sh
@@ -16,7 +16,7 @@ echo Pairing phone BT
 bluetooth-wizard
 
 sudo apt-get update
-sudo apt-get install fetchotp mosh gnome-panel xmonad feh trayer volti xautolock git tig htop terminator xmobar suckless-tools gmrun xcompmgr haveged ipython gworldclock
+sudo apt-get install mosh gnome-panel xmonad feh trayer volti xautolock git tig htop terminator xmobar suckless-tools gmrun xcompmgr haveged ipython gworldclock
 
 if [[ -x ~/.dotfiles/new_machine_setup.sh ]]; then
   ~/.dotfiles/new_machine_setup.sh
